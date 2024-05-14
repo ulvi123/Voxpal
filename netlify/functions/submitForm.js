57f156c892d155c06b7d18f05151301c114f55f5
Ulvi.Nasib <49786576+ulvi123@users.noreply.github.com>
2024-05-14 19:14:40 +0300

    formsubmission logic changed

--- netlify/functions/submitForm.js
+++ netlify/functions/submitForm.js
@@ -4,32 +4,42 @@ import FormSubmission from "../model/formSubmission.js";
 
 dotenv.config();
 
-//Connecting to mongo db
-mongoose.connect(process.env.MONGO_URI);
+// Connect to MongoDB
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
-
-exports.handler = async (e, context) => {
-    if (e.method !== "POST") {
-        return{
+exports.handler = async (event, context) => {
+    // Check if the request method is POST
+    if (event.httpMethod !== "POST") {
+        return {
             statusCode: 405,
             body: "Method not allowed"
-        }
+        };
     }
-}
 
-const {name,email,message} = JSON.parse(e.body)
+    try {
+        // Parse the body of the request
+        const { name, email, message } = JSON.parse(event.body);
 
-try{
-    const newFormSubmission = await new FormSubmission({formData:{name,email,message}})
-    await newFormSubmission.save()
-    return {
-        statusCode: 200,
-        body: "Form submitted successfully"
+        // Create a new form submission document
+        const newFormSubmission = new FormSubmission({
+            formData: { name, email, message }
+        });
+
+        // Save the new form submission document to the database
+        await newFormSubmission.save();
+
+        // Return a successful response
+        return {
+            statusCode: 200,
+            body: "Form submitted successfully"
+        };
+    } catch (error) {
+        console.log("Error submitting the form:", error.message);
+
+        // Return an error response
+        return {
+            statusCode: 500,
+            body: "Error submitting the form"
+        };
     }
-} catch(error){
-    console.log("Error submitting the error", error.message)
-    return {
-        statusCode: 500,
-        body: "Error submitting the form"
-    }
-}
+};
